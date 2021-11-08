8a36e387bbf22bda3c0e770a7e5fc91f4d9ada9c
Supper Balint <balintsupper@gmail.com>
2021-11-08 14:05:41 +0100

    Update automation script

--- launch.py
+++ launch.py
@@ -1,15 +1,17 @@
 import argparse
 import subprocess
 import pipes
+import os
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--file", help="Rout to the file, containing the list of video file names.")
 args = vars(ap.parse_args())
 
 with open(args["file"]) as f:
     for name in f:
-        name = f"/home/balint/Projects/KinemAutomation/app/jigsaws/Knot_Tying/video/{name}"
-        name1 = f"/home/balint/Projects/KinemAutomation/app/jigsaws/Knot_Tying/video/{next(f)}"
-    
+        name = f"{os.getcwd()}/app/jigsaws/Knot_Tying/video/{name}"
+        name1 = f"{os.getcwd()}/app/jigsaws/Knot_Tying/video/{next(f)}"
+
         finished = False
         while not finished:
             subprocess.run(["python3", "app/main.py", "-vl", pipes.quote(name.strip('\n')), "-vr", pipes.quote(name1.strip('\n'))])
